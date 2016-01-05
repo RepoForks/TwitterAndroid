4cc5d893815f9732d63c58e11b8ff36e28b66c60
Maarten <maarten.vangiel@student.kdg.be>
2016-01-05 15:32:04 +0100

    Getters en Setters van Tweet-klasse geïmplementeerd

--- app/src/main/java/be/kdg/twitterandroid/domain/Tweet.java
+++ app/src/main/java/be/kdg/twitterandroid/domain/Tweet.java
@@ -38,4 +38,228 @@ public class Tweet {
     private Tweet quoted_status, retweeted_status;
 
     private User user;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getIn_reply_to_status_id() {
+        return in_reply_to_status_id;
+    }
+
+    public void setIn_reply_to_status_id(long in_reply_to_status_id) {
+        this.in_reply_to_status_id = in_reply_to_status_id;
+    }
+
+    public long getIn_reply_to_user_id() {
+        return in_reply_to_user_id;
+    }
+
+    public void setIn_reply_to_user_id(long in_reply_to_user_id) {
+        this.in_reply_to_user_id = in_reply_to_user_id;
+    }
+
+    public long getQuoted_status_id() {
+        return quoted_status_id;
+    }
+
+    public void setQuoted_status_id(long quoted_status_id) {
+        this.quoted_status_id = quoted_status_id;
+    }
+
+    public boolean isFavorited() {
+        return favorited;
+    }
+
+    public void setFavorited(boolean favorited) {
+        this.favorited = favorited;
+    }
+
+    public boolean isPossibly_sensitive() {
+        return possibly_sensitive;
+    }
+
+    public void setPossibly_sensitive(boolean possibly_sensitive) {
+        this.possibly_sensitive = possibly_sensitive;
+    }
+
+    public boolean isRetweeted() {
+        return retweeted;
+    }
+
+    public void setRetweeted(boolean retweeted) {
+        this.retweeted = retweeted;
+    }
+
+    public boolean isTruncated() {
+        return truncated;
+    }
+
+    public void setTruncated(boolean truncated) {
+        this.truncated = truncated;
+    }
+
+    public boolean isWithheld_copyright() {
+        return withheld_copyright;
+    }
+
+    public void setWithheld_copyright(boolean withheld_copyright) {
+        this.withheld_copyright = withheld_copyright;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getFilter_level() {
+        return filter_level;
+    }
+
+    public void setFilter_level(String filter_level) {
+        this.filter_level = filter_level;
+    }
+
+    public String getId_str() {
+        return id_str;
+    }
+
+    public void setId_str(String id_str) {
+        this.id_str = id_str;
+    }
+
+    public String getIn_reply_to_screen_name() {
+        return in_reply_to_screen_name;
+    }
+
+    public void setIn_reply_to_screen_name(String in_reply_to_screen_name) {
+        this.in_reply_to_screen_name = in_reply_to_screen_name;
+    }
+
+    public String getIn_reply_to_status_id_str() {
+        return in_reply_to_status_id_str;
+    }
+
+    public void setIn_reply_to_status_id_str(String in_reply_to_status_id_str) {
+        this.in_reply_to_status_id_str = in_reply_to_status_id_str;
+    }
+
+    public String getIn_reply_to_user_id_str() {
+        return in_reply_to_user_id_str;
+    }
+
+    public void setIn_reply_to_user_id_str(String in_reply_to_user_id_str) {
+        this.in_reply_to_user_id_str = in_reply_to_user_id_str;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    public String getQuoted_status_id_str() {
+        return quoted_status_id_str;
+    }
+
+    public void setQuoted_status_id_str(String quoted_status_id_str) {
+        this.quoted_status_id_str = quoted_status_id_str;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getWithheld_in_countries() {
+        return withheld_in_countries;
+    }
+
+    public void setWithheld_in_countries(String withheld_in_countries) {
+        this.withheld_in_countries = withheld_in_countries;
+    }
+
+    public String getWithheld_scope() {
+        return withheld_scope;
+    }
+
+    public void setWithheld_scope(String withheld_scope) {
+        this.withheld_scope = withheld_scope;
+    }
+
+    public int getFavorite_count() {
+        return favorite_count;
+    }
+
+    public void setFavorite_count(int favorite_count) {
+        this.favorite_count = favorite_count;
+    }
+
+    public int getRetweet_count() {
+        return retweet_count;
+    }
+
+    public void setRetweet_count(int retweet_count) {
+        this.retweet_count = retweet_count;
+    }
+
+    public Entities getEntities() {
+        return entities;
+    }
+
+    public void setEntities(Entities entities) {
+        this.entities = entities;
+    }
+
+    public Place getPlace() {
+        return place;
+    }
+
+    public void setPlace(Place place) {
+        this.place = place;
+    }
+
+    public Tweet getQuoted_status() {
+        return quoted_status;
+    }
+
+    public void setQuoted_status(Tweet quoted_status) {
+        this.quoted_status = quoted_status;
+    }
+
+    public Tweet getRetweeted_status() {
+        return retweeted_status;
+    }
+
+    public void setRetweeted_status(Tweet retweeted_status) {
+        this.retweeted_status = retweeted_status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
