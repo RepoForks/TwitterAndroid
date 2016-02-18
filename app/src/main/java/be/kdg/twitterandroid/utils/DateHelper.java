b7baa31aea7bcb1a625b4c512e831ee41c3e4416
Maarten <maarten.vangiel@student.kdg.be>
2016-02-18 19:06:26 +0100

    Fixed issue where 60 minutes ago was shown as "0 wk"

--- app/src/main/java/be/kdg/twitterandroid/utils/DateHelper.java
+++ app/src/main/java/be/kdg/twitterandroid/utils/DateHelper.java
@@ -21,10 +21,10 @@ public final class DateHelper {
             Date now = new Date();
             long delta = (now.getTime() - date.getTime()) / (1000 * 60); // delta in minutes
 
-            if(delta < 1) return "now";
+            if(delta <= 1) return "now";
             else if(delta > 1 && delta < 60) return delta + "m";
-            else if(delta > 60 && delta < 24 * 60) return delta / 60 + "h";
-            else if(delta > 24 * 60 && delta < 24 * 60 * 7) return delta / 60 / 24 + "d";
+            else if(delta >= 60 && delta < 24 * 60) return delta / 60 + "h";
+            else if(delta >= 24 * 60 && delta < 24 * 60 * 7) return delta / 60 / 24 + "d";
             else return delta / 60 / 24 / 7 + "wk";
 
         } catch (ParseException e) {
